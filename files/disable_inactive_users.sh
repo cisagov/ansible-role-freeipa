d4abf811424eba86bce20052222e249ef20b3ffc
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-07-23 13:07:33 -0400

    Add a new and improved script to disable inactive users
    The old script did not actually work as intended.

---
+++ files/disable_inactive_users.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/bash
+
+# Usage:
+#   disable_inactive_users.sh [disable_time]
+#
+# A user is considered inactive if he or she has not logged since
+# disable_time.  If disable_time is not specified then a default value
+# of "45 days ago" is used.
+#
+# The date/time must be specified so that it understandable by the
+# date command, which is the same as saying it must be understandable
+# by coreutils.  See the "Date input formats" section of the info
+# manual distributed with coreutils for more information on date/time
+# formats:
+# https://www.gnu.org/software/coreutils/manual/html_node/Date-input-formats.html
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# A user is considered inactive if he or she has not logged since this
+# time.
+#
+# See the "Shell Parameter Expansion" section of the info manual
+# distributed with bash for more details on this form of parameter
+# assignment:
+# https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+disable_time=${1:-45 days ago}
+
+function usage {
+  cat << HELP
+Usage:
+  ${0##*/} [disable_time]
+
+A user is considered inactive if he or she has not logged since
+disable_time.  If disable_time is not specified then a default value
+of "45 days ago" is used.
+
+The date/time must be specified so that it understandable by the date
+command, which is the same as saying it must be understandable by
+coreutils.  See the "Date input formats" section of the info manual
+distributed with coreutils for more information on date/time formats:
+https://www.gnu.org/software/coreutils/manual/html_node/Date-input-formats.html
+HELP
+  exit 1
+}
+
+if [ $# -gt 1 ]; then
+  usage
+else
+  # Convert disable_time into an integer representing seconds since
+  # the epoch.
+  disable_deadline=$(date --date="$disable_time" +%S)
+
+  # Grab a list of all non-disabled FreeIPA users
+  users=$(
+    # Note that we disable the size and time limits normally in place
+    # when running ipa user-find.
+    ipa user-find --disabled=false --sizelimit=0 --timelimit=0 \
+      |
+      # --quiet means no printing unless the p command is used
+      sed --quiet 's/^\s*User login:\s*//p'
+  )
+
+  for user in $users; do
+    timestamps=$(ipa user-status "$user" \
+      |
+      # --quiet means no printing unless the p command is used
+      sed --quiet 's/\s*Last successful authentication:\s*//p')
+    # In the event that all timestamps are invalid (N/A) we don't want
+    # to disable the user since we don't know the last time he or she
+    # authenticated.
+    #
+    # TODO: After disable_time has elapsed with the "KDC:Disable Last
+    # Success" feature disabled we can start deleting users with all
+    # authentication timestamps invalid (N/A).  See #74 for more
+    # details.
+    all_timestamps_invalid=true
+    disable_user=true
+    for timestamp in $timestamps; do
+      # Do we have a valid timestamp in the format that the command
+      # ipa user-status uses (YYYYMMDDHHMMSSZ)?
+      if [[ $timestamp =~ ^([[:digit:]]{4})([[:digit:]]{2})([[:digit:]]{2})([[:digit:]]{2})([[:digit:]]{2})([[:digit:]]{2})Z$ ]]; then
+        # At least one timestamp is valid.
+        all_timestamps_invalid=false
+
+        # 1. Reformat the timestamp to ISO 8601 format so the date
+        # command can understand it.
+        # 2. Use the date command to convert the ISO 8601 timestamp to
+        # seconds since the epoch.
+        timestamp=${BASH_REMATCH[1]}-${BASH_REMATCH[2]}-${BASH_REMATCH[3]}T${BASH_REMATCH[4]}:${BASH_REMATCH[4]}:${BASH_REMATCH[6]}Z
+        timestamp=$(date --date="$timestamp" +%s)
+
+        # If the timestamp postdates the deadline then the user is
+        # active and should not be disabled.
+        if ((timestamp > disable_deadline)); then
+          disable_user=false
+        fi
+      fi
+    done
+
+    # Now that we have analyzed the last authentication timestamps,
+    # disable the user if necessary.
+    #
+    # TODO: Note also that after disable_time has elapsed with the
+    # "KDC:Disable Last Success" feature disabled we can start
+    # deleting users with all authentication timestamps invalid (N/A)
+    # and simplify this logic.  See #74 for more details.
+    if $disable_user; then
+      if ! $all_timestamps_invalid; then
+        # ipa user-disable $user
+        echo User "$user" disabled because he or she is an inactive user.
+      else
+        echo User "$user" not disabled because all authentication timestamps were invalid.
+      fi
+    else
+      echo User "$user" not disabled because he or she is an active user.
+    fi
+  done
+fi
