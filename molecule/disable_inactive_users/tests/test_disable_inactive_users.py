5259e0100838181460fc475ff11032c56c8199af
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-07-26 10:33:55 -0400

    Remove unnecessary test
    This test was erroneously left over from a previous copy and paste.

--- molecule/disable_inactive_users/tests/test_disable_inactive_users.py
+++ molecule/disable_inactive_users/tests/test_disable_inactive_users.py
@@ -40,23 +40,3 @@ def test_verify_systemd_units(host, f):
     """Verify systemd can parse the unit files we created."""
     cmd = host.run(f"systemd-analyze verify {f}")
     assert cmd.rc == 0
-
-
-def test_apache_config(host):
-    """Verify Apache can parse the config with the additions we made."""
-    # The ssl.conf file is invalid until FreeIPA's self-signed
-    # certificate is present.  This can only be true after
-    # ipa-server-install has been run, so we need to temporarily move
-    # this file out of the way.  This does not affect the results of
-    # the test, since we are not touching ssl.conf anyway.
-    cmd = host.run("mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.xxx")
-    assert cmd.rc == 0
-
-    # Now verify that Apache is OK with the configuration _with_ the
-    # modification(s) we have made.
-    cmd = host.run("apachectl configtest")
-    assert cmd.rc == 0
-
-    # Put back the ssl.conf file.
-    cmd = host.run("mv /etc/httpd/conf.d/ssl.conf.xxx /etc/httpd/conf.d/ssl.conf")
-    assert cmd.rc == 0
